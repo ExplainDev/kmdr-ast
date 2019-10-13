edf39847472d3dc2caadcf7dff68019491fe973b
Eddie Ramirez <djedi.r@gmail.com>
2019-10-13 09:45:01 -0700

    Remove dist/

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { AssignmentNode, ArgumentNode, CommandLeafNodes, CommandNode, CompoundNode, FlatAST, ListLeafNodes, ListNode, NodeAST, OperatorNode, OptionNode, OptionWithArgNode, PipeNode, PipelineNode, ProgramNode, RedirectNode, ReservedWordNode, StickyOptionNode, SubcommandNode, SudoNode, WordNode } from "./interfaces";
-import AST from "./ast";
-export { AssignmentNode, ArgumentNode, CommandLeafNodes, CommandNode, CompoundNode, FlatAST, ListLeafNodes, ListNode, NodeAST, OperatorNode, OptionNode, OptionWithArgNode, PipeNode, PipelineNode, ProgramNode, RedirectNode, ReservedWordNode, StickyOptionNode, SubcommandNode, SudoNode, WordNode };
-export default AST;
-//# sourceMappingURL=index.d.ts.map
